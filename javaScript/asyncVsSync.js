7e14a4b2c004147e6a005a2f84a13aa56621ee44
Aman <amanyadav592002@gmail.com>
2022-05-19 23:14:49 +0530

    asynchronous programming learnt the basics

---
+++ javaScript/asyncVsSync.js
@@ -0,0 +1,11 @@
+console.log("javaScript is successfully connnected!")
+
+setTimeout(() => {
+    for (let i = 0; i < 5; i++) {
+    const element = 5;
+    console.log("printing value of i after 1000 milisecond "+i)
+}
+}, 1000);
+
+document.write('i wrote this with help of js by asynchronous prograaming.') 
+
