bf161930a8563a1557ec031b9296f45f667aa8b9
Aman <amanyadav592002@gmail.com>
2022-05-24 00:22:47 +0530

    ajax practice

--- javaScript/ajax.js
+++ javaScript/ajax.js
@@ -6,21 +6,21 @@ fetchBtn.addEventListener('click', buttonCLickHandler)
 function buttonCLickHandler() {
     console.log("you have clicked the fetchbtn")
     //instantiate(create) an xhr object
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
     //open the object here true is for asynchronous; get-> getting the data post-> posting the data  if we used false then the last conosle that is in line 32 will be executed last instead of earlietr
     //USE THIS FOR GET request
     // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
 
     //USE THIS FOR POST request
-    xhr.open('POST', '	https://dummy.restapiexample.com/api/v1/create', true);
-    xhr.getResponseHeader('Content-type','application/json');
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true);
+    xhr.getResponseHeader('Content-type', 'application/json');
 
     //onload and progress can be used for spinners for giving the feel of loading something for the user.
     //what to do on progress its up to you whether youuse or not
-    xhr.onprogress = function () {
-        console.log("on progress")
-    }
+    // xhr.onprogress = function () {
+    //     console.log("on progress")
+    // }
 
     //what to do when response is ready , we can also check status in onload if file is not found or found
     xhr.onload = function () {
@@ -33,38 +33,38 @@ function buttonCLickHandler() {
     }
 
     //we need to send the request also. 
-     params=`{"name":"test","salary":"123","age":"23"}`;
+    params = `{"name":"test34sad545","salary":"123","age":"23"}`;
     xhr.send(params);
     console.log("we are done.")
-
-
-
-
-
-
 }
 
 let popBtn = document.getElementById("popBtn");
-popBtn.addEventListener('click',popHandler);
+popBtn.addEventListener('click', popHandler);
 
-function popHandler(){
+function popHandler() {
     console.log("you have clicked the pop button")
 
     //instantiate the xhr object
-    let xhr=new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
 
     //open the object 
-    xhr.open('GET','	https://dummy.restapiexample.com/api/v1/employees',true);
+    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
 
-    xhr.onload=function(){
+    xhr.onload = function () {
         if (this.status === 200) {
-            let obj=JSON.parse(this.responseText)
-            console.log(obj)
+            let obj = JSON.parse(this.responseText)
+            console.log(obj);
+            let str="";
+            let list=document.getElementById('list');
+            for(key in obj){
+                str += `<li>${obj[key]}</li>`;
+            }
+            list.innerHTML=str;
         } else {
             console.error("some error occured")
         }
     }
 
     xhr.send();
-    console.log("we are done")
+    console.log("we are done fetching employee!")
 }
---
+++ javaScript/ajaxPractice.js
@@ -0,0 +1,53 @@
+console.log("javaScript is connected.");
+
+//everything done but unable to append the list. 
+
+let vegetableBtn=document.getElementById('vegetableBtn');
+vegetableBtn.addEventListener('click',listner);
+
+function listner(){
+    console.log("you have clicked the vegetable button.")
+    const xhr= new XMLHttpRequest();
+
+    xhr.open('GET','vegetable.json',true);
+    xhr.onload=function(){
+        console.log(this.responseText)
+        if (this.status===200) {
+            let obj=JSON.parse(this.responseText);
+            console.log(obj);
+            let str="";
+            let vegetableList=document.getElementById('vegetableList');
+            for(key in obj){
+                str += `<li>${obj[key]}</li>`;
+            }
+            vegetableList.innerHTML=str;
+        } else {
+            console.error("some error occured")
+        }
+    }
+    xhr.send();
+}
+
+let fruitBtn=document.getElementById('fruitBtn');
+fruitBtn.addEventListener('click',flistner);
+
+function flistner(){
+    console.log("you have clicked the fruit button.")
+    const xhr= new XMLHttpRequest();
+
+    xhr.open('GET','fruits.json',true);
+    xhr.onload=function(){
+        console.log(this.responseText)
+        if (this.status===200) {
+            let obj=JSON.parse(this.responseText);
+            console.log(obj);
+            let fruitstr=`<li>${obj}</li>`;
+            let fruitList=document.getElementById('fruitList');
+        
+            fruitList.innerHTML= fruitstr;
+        } else {
+            console.error("some error occured")
+        }
+    }
+    xhr.send();
+}
