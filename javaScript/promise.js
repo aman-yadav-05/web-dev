600db12b54c257f55c8fe1024e26ff501649c553
Aman <amanyadav592002@gmail.com>
2022-05-29 14:41:05 +0530

    daily comits

---
+++ javaScript/promise.js
@@ -0,0 +1,45 @@
+console.log("promise javascript is successfully connected!");
+// promises can be used instead of callback function 
+
+// the below code is from callBackFunction.js 
+
+const subjects = [
+    { name: "computer system architecture", teacher: "pushplata pujari" },
+    { name: "design and analysis of algorithm", teacher: "abhisek patel" }
+]
+
+function addsub(subject) {
+    return new Promise(function(resolve,reject){
+        setTimeout(() => {
+            subjects.push(subject);
+            // callback();
+            error=false;
+            if(!error){
+                resolve();
+            }else{
+                reject();
+            }
+            console.log("subject is added.")
+        }, 5000);
+    })
+}
+
+function getList() {
+    setTimeout(() => {
+        let string = "";
+        subjects.forEach(function (subject) {
+            string += `<li>${subject.name} taught by ${subject.teacher}.</li>`;
+        })
+        document.getElementById("list").innerHTML = string;
+        console.log("student have been fetched")
+    }, 1000);
+}
+
+let newSubject = { name: "javaScript", teacher: "NA" }
+addsub(newSubject).then(getList).catch(function(){
+    conosle.log("some error occured.")
+});
+
+// the function inside the then is resolve function and the function inside the catch is reject function 
+
+// then(resolve) , catch(reject )
