9006731f8cfb5c138c32b45609a54e32db5b00a6
Aman <amanyadav592002@gmail.com>
2022-09-14 20:29:40 +0530

    updated some values

---
+++ javaScript/metaSymbols.js
@@ -0,0 +1,6 @@
+console.log('javascript is connected.')
+ let check= /^aman/;
+ let string= ` aman this is string to check the metacharacters symbol in javascript. aman`;
+
+let result= check.exec(string);
+console.log(result);
